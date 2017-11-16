ebfff1432360fc9b91268225d2d956b6b0ad2166
tim-nguyen-OK <timothy.nguyen@okstate.edu>
2017-11-15 22:55:50 -0600

    Tim - menuCategories.php and menuCategory.php have been editted to query the database and display the selections using a multidimensional array that can be concatinated onto the HTML document.

--- item.php
+++ item.php
@@ -1,9 +1,16 @@
-<<<<<<< HEAD
+
 <?php
-/**
-max ingredient length 11 chars
-**/
-$item = [1];
+
+$dbc = mysqli_connect('localhost','root','','drinkdrankdrunk')
+or die('Error connecting to MySQL server.');
+$query = 'SELECT * FROM menu_items WHERE menuitem_id = "'.$_GET['itemId'].'"';
+$result = mysqli_query($dbc, $query) or die('Error querying database.');
+mysqli_close($dbc);
+
+
+$item = mysqli_fetch_all($results);
+echo $item;
+
 echo 	'<div id = "Menu" class = "Menu">
 			<div id = "Restaurants" class = "MenuItem" onclick = "getRestaurants();">Restaurants</div>
 			<div class = "MenuItem">Login</div>
@@ -20,26 +27,3 @@ echo 	'<div id = "Menu" class = "Menu">
 			</table>
 		</div>';
 ?>
-=======
-<?php
-/**
-max ingredient length 11 chars
-**/
-$item = [1];
-echo 	'<div id = "Menu" class = "Menu">
-			<div id = "Restaurants" class = "MenuItem" onclick = "getRestaurants();">Restaurants</div>
-			<div class = "MenuItem">Login</div>
-			<div class = "MenuItem"></div>
-		</div>
-		<div class = "ItemImage First" alt = ' . $item[0] . ' style = "background-image: url(' . $item[0] . '); background-size: 100% 100%;">' . $item[0] . '</div>
-		<div class = "ItemIngredientsContainer">
-			<table style = "width: 100%">
-				<caption class = "IngredientsTitle">Ingredients</caption>
-				<tr>
-					<td class = "Ingredient">XXXXXXXX</td>
-					<td class = "Ingredient">XXXXXXXX</td>
-				</tr>
-			</table>
-		</div>';
-?>
->>>>>>> bad79d4238d400074b193b2d035dcdf5d454045b
